322e65fecd5e4e176c7d50afbf7b43bc8687be94
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-05-30 00:44:39 +0200

    make testing faster

--- src/library/evaluator/evaluator_tests.rs
+++ src/library/evaluator/evaluator_tests.rs
@@ -49,35 +49,35 @@ macro_rules! should_be_equal_parsed {
 #[test]
 fn text_evalaution_of_integers() {
     current_allocation_counting!();
-    should_be_integer_equal_to(eval_input("0"), 0);
-    should_be_integer_equal_to(eval_input("1"), 1);
-    should_be_integer_equal_to(eval_input("3"), 3);
+    should_be_integer_equal_to("0", 0);
+    should_be_integer_equal_to("1", 1);
+    should_be_integer_equal_to("3", 3);
 }
 
 #[test]
 fn text_evaluation_of_booleans() {
-    should_be_boolean_equal_to(eval_input("true"), true);
-    should_be_boolean_equal_to(eval_input("false"), false);
+    should_be_boolean_equal_to("true", true);
+    should_be_boolean_equal_to("false", false);
 }
 
 #[test]
 fn text_evaluation_of_strings() {
-    should_be_string_equal_to(eval_input("\"Hello, World!\""), "Hello, World!".to_string());
-    should_be_string_equal_to(eval_input("\"Test String\""), "Test String".to_string());
+    should_be_string_equal_to("\"Hello, World!\"", "Hello, World!".to_string());
+    should_be_string_equal_to("\"Test String\"", "Test String".to_string());
 }
 
 #[test]
 fn test_bang_operator() {
-    should_parsed_be_boolean_equal_to("!\"\"", true);
-    should_parsed_be_boolean_equal_to("!\"  \"", true);
-    should_parsed_be_boolean_equal_to("!\"xx\"", false);
-    should_parsed_be_boolean_equal_to("!!\"xx\"", true);
-    should_parsed_be_boolean_equal_to("!5", false);
-    should_parsed_be_boolean_equal_to("!0", false);
-    should_parsed_be_boolean_equal_to("!!0", true);
-    should_parsed_be_boolean_equal_to("!!5", true);
-    should_parsed_be_boolean_equal_to("!!true", true);
-    should_parsed_be_boolean_equal_to("!!false", false);
+    should_be_boolean_equal_to("!\"\"", true);
+    should_be_boolean_equal_to("!\"  \"", true);
+    should_be_boolean_equal_to("!\"xx\"", false);
+    should_be_boolean_equal_to("!!\"xx\"", true);
+    should_be_boolean_equal_to("!5", false);
+    should_be_boolean_equal_to("!0", false);
+    should_be_boolean_equal_to("!!0", true);
+    should_be_boolean_equal_to("!!5", true);
+    should_be_boolean_equal_to("!!true", true);
+    should_be_boolean_equal_to("!!false", false);
 }
 
 fn eval_input(input: &str) -> Object {
@@ -87,20 +87,33 @@ fn eval_input(input: &str) -> Object {
     evaluate(&program)
 }
 
-fn should_be_integer_equal_to(left: Object, right: i64) {
-    should_be_equal!(left, right, Int);
+fn should_be_integer_equal_to(left: &str, right: i64) {
+    should_be_equal_parsed!(left, right, Int);
 }
 
-fn should_parsed_be_boolean_equal_to(left: &str, right: bool) {
+fn should_be_boolean_equal_to(left: &str, right: bool) {
     should_be_equal_parsed!(left, right, Boolean);
 }
 
-fn should_be_boolean_equal_to(left: Object, right: bool) {
-    should_be_equal!(left, right, Boolean);
-}
-
-fn should_be_string_equal_to(left: Object, right: String) {
-    should_be_equal!(left, right, String);
+fn should_be_string_equal_to(left: &str, right: String) {
+    let left = eval_input(left);
+    let left_string = left.to_string();
+    match left {
+        Object::String(value) => assert_eq!(
+            value,
+            right,
+            "Expected String with value {}, but got {} for input {}",
+            right.to_string(),
+            left_string,
+            left_string
+        ),
+        _ => panic!(
+            "Expected String with value {}, but got {} for input {}",
+            right.to_string(),
+            left.to_string(),
+            left.to_string()
+        ),
+    };
 }
 
 fn check_parser_errors(parser: &Parser) {
