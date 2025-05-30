8f80ee1c6a92ef76e1b4dcaaf7324a1a2de28ec0
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-05-30 17:43:16 +0200

    infix operators

--- src/library/evaluator/mod.rs
+++ src/library/evaluator/mod.rs
@@ -4,7 +4,7 @@ use crate::{
     allocation_counting,
     ast::{
         base::Node,
-        expression::{Expression, PrefixOperatorType},
+        expression::{Expression, InfixOperatorType, PrefixOperatorType},
         statements::{Program, Statement},
     },
     object::Object,
@@ -65,7 +65,161 @@ fn evaluate_expression(expression: &Expression) -> Object {
             operator,
             right,
         } => prefix_operator_evaluation(token, operator, right.as_ref()),
+        Expression::InfixExpression {
+            token,
+            left,
+            operator,
+            right,
+        } => {
+            let left_value = evaluate_expression(left);
+            let right_value = evaluate_expression(right);
+            return infix_operator_evaluation(token, operator, left_value, right_value);
+        }
         _ => panic!("Expression type not implemented: {:?}", expression),
+    }
+}
+
+fn infix_operator_evaluation(
+    token: &Token,
+    operator: &InfixOperatorType,
+    left: Object,
+    right: Object,
+) -> Object {
+    let some_value: Option<Object> = match left {
+        Object::Int(left_value) => match right {
+            Object::Int(right_value) => Some(infix_operator_evaluation_int(
+                token,
+                operator,
+                left_value,
+                right_value,
+            )),
+            Object::String(ref right_value) => Some(int_to_string_infix_evaluation(
+                token,
+                operator,
+                left_value,
+                right_value,
+            )),
+            _ => None,
+        },
+        Object::String(ref left_value) => match right {
+            Object::Int(right_value) => Some(string_to_int_infix_evaluation(
+                token,
+                operator,
+                left_value,
+                right_value,
+            )),
+            Object::String(ref right_value) => Some(string_infix_evaluation(
+                token,
+                operator,
+                left_value,
+                right_value,
+            )),
+            _ => None,
+        },
+        Object::Boolean(_) => None,
+        Object::Null => None,
+    };
+    return some_value.unwrap_or_else(|| {
+        panic!(
+            "Infix operator evaluation failed for token: {} with left value: {} and right value: {}",
+            token.to_string(),
+            left.to_string(),
+            right.to_string()
+        )
+    });
+}
+
+fn string_infix_evaluation(
+    token: &Token,
+    operator: &InfixOperatorType,
+    left: &String,
+    right: &String,
+) -> Object {
+    match operator {
+        InfixOperatorType::Plus => {
+            let result = format!("{}{}", left, right);
+            return string_value(result);
+        }
+        InfixOperatorType::Multiply => {
+            let mut result = String::new();
+            for _ in 0..right.parse::<usize>().unwrap_or(0) {
+                result.push_str(left);
+            }
+            return string_value(result);
+        }
+        _ => panic!(
+            "Infix operator evaluation not implemented for strings: {:?} for token: {}",
+            operator,
+            token.to_string()
+        ),
+    }
+}
+fn int_to_string_infix_evaluation(
+    token: &Token,
+    operator: &InfixOperatorType,
+    left_value: i64,
+    right_value: &str,
+) -> Object {
+    match operator {
+        InfixOperatorType::Plus => {
+            let result = format!("{}{}", left_value, right_value);
+            return string_value(result);
+        }
+        _ => panic!(
+            "Infix operator evaluation not implemented for string and integer: {:?} for token: {}",
+            operator,
+            token.to_string()
+        ),
+    }
+}
+
+fn string_to_int_infix_evaluation(
+    token: &Token,
+    operator: &InfixOperatorType,
+    left: &String,
+    right_value: i64,
+) -> Object {
+    match operator {
+        InfixOperatorType::Plus => {
+            let result = format!("{}{}", left, right_value);
+            return string_value(result);
+        }
+        InfixOperatorType::Multiply => {
+            let mut result = String::new();
+            for _ in 0..right_value {
+                result.push_str(left);
+            }
+            return string_value(result);
+        }
+        _ => panic!(
+            "Infix operator evaluation not implemented for string and integer: {:?} for token: {}",
+            operator,
+            token.to_string()
+        ),
+    }
+}
+
+fn infix_operator_evaluation_int(
+    token: &Token,
+    operator: &InfixOperatorType,
+    left_value: i64,
+    right_value: i64,
+) -> Object {
+    match operator {
+        InfixOperatorType::Plus => int_value(left_value + right_value),
+        InfixOperatorType::Minus => int_value(left_value - right_value),
+        InfixOperatorType::Multiply => int_value(left_value * right_value),
+        InfixOperatorType::Divide => {
+            if right_value == 0 {
+                panic!("Division by zero error at {}", token.at_text());
+            }
+            int_value(left_value / right_value)
+        }
+        _ => panic!(
+            "Infix operator evaluation not implemented: {:?} for token: {}",
+            operator,
+            token.to_string()
+        ),
     }
 }
 
