cd777948177d9d1f80077bed2de3f4f26aefdb72
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-11-15 07:50:55 +0100

    More tests

--- src/library/vm/testing/functions_with_arguments.rs
+++ src/library/vm/testing/functions_with_arguments.rs
@@ -7,9 +7,48 @@ generate_vm_tests! {
     indentity: (r#"
     let identity = fn(a){a}
     identity(4)
-    "#, should_be_integer(4)),
+    "#,should_be_integer(4)),
     addition: (r#"
     let add = fn(a,b){a+b}
     add(1,2)
     "#, should_be_integer(3)),
+    addition_with_let: (r#"
+    let add = fn(a,b){
+      let c = a+b;
+      c;
+    }
+    add(1,2)
+    "#, should_be_integer(3)),
+    complex_addition: (r#"
+    let add = fn(a,b){
+      let c = a+b;
+      c;
+    }
+    add(1,2) + add(3,4)
+    "#, should_be_integer(10)),
+
+    complex_addition_wrapped: (r#"
+    let add = fn(a,b){
+      let c = a+b;
+      c;
+    }
+    let wrapper = fn(){
+      add(1,2) + add(3,4)
+    }
+    wrapper()
+    "#, should_be_integer(10)),
+
+    complex_addition_wrapped_with_global: (r#"
+    let global = 100
+    let add = fn(a,b){
+      let c = a+b;
+      c;
+    }
+    let wrapper = fn(){
+      add(1,2) + add(3,4) + global
+    }
+    wrapper() + global
+    "#, should_be_integer(210)),
+
+
 }
