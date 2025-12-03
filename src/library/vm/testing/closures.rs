ef16aea106f9f248113c225285367912ae41b6d1
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-12-03 06:59:23 +0100

    More tests for closures

--- src/library/vm/testing/closures.rs
+++ src/library/vm/testing/closures.rs
@@ -9,4 +9,19 @@ generate_vm_tests! {
     let closure = closureFactory(5);
     closure()
     "#, should_be_integer(5)),
+    more_complex: (r#"
+    let global = 1000;
+    let closureFactory = fn(a){ fn(b) { fn(c) { a + b + c +global } } };
+    let closure = closureFactory(100)(10);
+    closure(1)
+    "#, should_be_integer(1111)),
+    combination: (r#"
+    let closureFactory = fn(a,b){ 
+        let one = fn() { a; } 
+        let two = fn() { b; } 
+        fn() { one() + two() }
+    };
+    let closure = closureFactory(10,1);
+    closure()
+    "#, should_be_integer(11)),
 }
